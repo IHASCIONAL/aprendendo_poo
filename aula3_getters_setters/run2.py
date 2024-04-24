5de1d69980503e401f1ceb091639040c8b7f55b2
Ismael <ismaelhenriqueeu@gmail.com>
2024-04-24 19:10:58 -0300

    Getters and Setters

---
+++ aula3_getters_setters/run2.py
@@ -0,0 +1,22 @@
+class MinhaClasse:
+    def __init__(self) -> None:
+        self.__valor = None
+        self.__elem = None
+    
+    def setter_valor(self, valor: int) -> None:
+        self.__valor = valor
+    
+    def setter_elem(self, elem: str) -> None:
+        self.__elem = elem
+
+    def getter_valor(self) -> int:
+        return self.__valor
+    
+    @property
+    def getter_elem(self) -> str:
+        return self.__elem
+
+my_class = MinhaClasse()
+my_class.setter_valor(123)
+print(my_class.getter_valor)
+
